7a5bb9d5be72db881a1cfbdf89868c684fde29b9
NelsonWilliam237 <keleko.nelson@gmail.com>
2025-10-08 15:09:48 -0400

    Commit question 5

--- Exercice1.dart
+++ Exercice1.dart
@@ -1,4 +1,10 @@
 class Couleur {
   int r, v, b;
   Couleur(this.r, this.v, this.b);
+  Couleur.rouge() : r = 255, v = 0, b = 0;
+  Couleur.vert() : r = 0, v = 255, b = 0;
+  Couleur.bleu() : r = 0, v = 0, b = 255;
+  void afficherCouleur() {
+    print("R: $r,V :$v,B:$b");
+  }
 }
