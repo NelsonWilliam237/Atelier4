1f2b30a2ff54ced8d6da84184013c65338d0f6d2
NelsonWilliam237 <keleko.nelson@gmail.com>
2025-10-15 14:05:58 -0400

    2.4

--- Exercice2.dart
+++ Exercice2.dart
@@ -3,4 +3,13 @@ class Point {
   double _y = 0;
   double get x => _x;
   double get y => _y;
+
+  void move(dx, dy) {
+    _x += dx;
+    _y += dy;
+  }
+
+  void display() {
+    print("les coordonnees sont ($x,$y)");
+  }
 }
