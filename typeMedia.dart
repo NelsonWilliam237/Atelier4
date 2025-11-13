b65ed998d0a322d76b51c9208868ae7e22947096
NelsonWilliam237 <keleko.nelson@gmail.com>
2025-11-13 10:08:01 -0500

    maj

--- typeMedia.dart
+++ typeMedia.dart
@@ -10,4 +10,18 @@ class Media {
 class Livre extends Media {
   final String auteur;
   Livre(super.titre, this.auteur);
+  @override
+  void afficherType() {
+    print("Ceci est un livre : $titre par $auteur");
+  }
+}
+
+class Film extends Media {
+  final int dureeMinutes;
+  Film(super.titre, this.dureeMinutes);
+
+  @override
+  void afficherType() {
+    print("Ceci est un Film : '$titre' d’une durée de $dureeMinutes minutes.");
+  }
 }
