9c25df7598257d8e2ae397dd43feff692054d6eb
NelsonWilliam237 <keleko.nelson@gmail.com>
2025-11-13 09:48:15 -0500

    maj

--- compteBancaire.dart
+++ compteBancaire.dart
@@ -4,4 +4,13 @@ class Compte {
 
   Compte(this.numeroCompte, [this._solde = 0.0]);
   String get solde => "$_solde.toStringAsfixed(2) \$";
+
+  void depot(double montant) {
+    if (montant > 0) {
+      _solde += montant;
+      print("Depot de ${montant.toStringAsFixed(2)} \$ effectue");
+    } else {
+      print("Le montant du depot doit etre positif");
+    }
+  }
 }
