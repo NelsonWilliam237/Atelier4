7c0376bea4881ee18792793b4b78b23c70e1a81a
NelsonWilliam237 <keleko.nelson@gmail.com>
2025-11-13 10:05:31 -0500

    maj

--- typeMedia.dart
+++ typeMedia.dart
@@ -1,4 +1,13 @@
 class Media {
   final String titre;
   Media(this.titre);
+
+  void afficherType() {
+    print("Ceci est un media generique");
+  }
+}
+
+class Livre extends Media {
+  final String auteur;
+  Livre(super.titre, this.auteur);
 }
