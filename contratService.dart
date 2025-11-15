d0030fd621140cb8f225d812bc53f5c8aa45273e
NelsonWilliam237 <keleko.nelson@gmail.com>
2025-11-14 22:11:38 -0500

    maj

--- contratService.dart
+++ contratService.dart
@@ -2,3 +2,15 @@ abstract class Connectable {
   void connecter(String utilisateur);
   void deconnecter();
 }
+
+class ServeurApi implements Connectable {
+  @override
+  void connecter(String utilisateur) {
+    print("ServeurApi : Connexion etablie pour $utilisateur");
+  }
+
+  @override
+  void deconnecter() {
+    print("ServeurApi : Deconnexion reussie");
+  }
+}
