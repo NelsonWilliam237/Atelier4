5a10454187c7c3e5926224a591849e0207f59487
NelsonWilliam237 <keleko.nelson@gmail.com>
2025-11-14 22:32:11 -0500

    maj

--- compteurTaches.dart
+++ compteurTaches.dart
@@ -1,4 +1,16 @@
 class Tache {
   String? description;
   static int nombreTotal = 0;
+
+  Tache(this.description) {
+    nombreTotal++;
+  }
+}
+
+void main() {
+  Tache t1 = Tache("Faire les devoirs");
+  Tache t2 = Tache("finaliser les projects");
+  Tache t3 = Tache("dormir");
+
+  print("le Total des taches est de : ${Tache.nombreTotal}");
 }
